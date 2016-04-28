0c6f26a19f7bfad38debe680a36f3fe87c29b5e0
LeonardooBorges <leo_bm08@hotmail.com>
2016-04-27 22:22:15 -0300

    Ta ai
    Falta corrigir uns problemas!

---
+++ Batalha.java
@@ -0,0 +1,60 @@
+
+class Batalha {
+	
+	public static void Atacando(Treinador t, Pokemon adversario){
+		System.out.println(t.pokemonAtual.nome+" usou "+t.ataqueAtual+" causando "+t.dano+" de dano!");
+		adversario.ferido(t.dano);
+	}
+	
+	public static void main(String[] args){
+		
+		boolean flag = true;
+		int escolha1,escolha2;
+		Treinador1 t1 = new Treinador1("Leo");
+		Treinador2 t2 = new Treinador2("Mit");
+		
+		
+		t1.euEscolhoVoce();
+		t2.euEscolhoVoce();
+		t1.defineAtaque(t2.pokemonAtual);
+		t2.defineAtaque(t1.pokemonAtual);
+		
+		while(flag){
+			escolha1 = t1.oQueFarei();
+			escolha2 = t2.oQueFarei();
+			
+			if(escolha1 == 0){
+				System.out.println("O treinador "+t2.nome+" venceu a partida!");
+				flag = false;
+			}
+			
+			else if(escolha2 == 0){
+				System.out.println("O treinador "+t1.nome+" venceu a partida!");
+				flag = false;
+			}
+			
+			else{
+				
+				if(escolha1 == 1){
+					t2.defineAtaque(t1.pokemonAtual);
+				}
+				
+				if(escolha2 == 1){
+					t1.defineAtaque(t2.pokemonAtual);
+				}
+				
+				if(escolha1 == 3){
+					Batalha.Atacando(t1, t2.pokemonAtual);
+				}
+				
+				if(escolha2 == 3){
+					Batalha.Atacando(t2, t1.pokemonAtual);
+
+				}
+				
+			}
+			
+		}
+		
+	}
+}
---
+++ Treinador.java
@@ -0,0 +1,97 @@
+
+abstract class Treinador {
+	
+	String nome;
+	Pokemon[] pokemons = new Pokemon[6];
+	
+	Pokemon pokemonAtual;
+	int nxt = 0;
+	String ataqueAtual;
+	int dano;
+	
+	double efectividade[][] = {
+			{1,   1,   1,   1,   1,  1,   1,   1},//NORMAL
+			{1, 0.5, 0.5,   1,   2,  1,   1,   1},//FIRE
+			{1,   2, 0.5,   1, 0.5,  1,   1,   1},//WATER
+			{1,   1,   2, 0.5, 0.5,  1,   2,   1},//ELETRIC
+			{1, 0.5,   2,   1, 0.5,  1, 0.5,   1},//GRASS
+			{2,   1,   1,   1,   1,  1, 0.5, 0.5},//FIGHTING
+			{1,   1,   1, 0.5,   2,  2,   1,   1},//FLYING
+			{1,   1,   1,   1,   1,  2,   1, 0.5} //PSYCHIC
+		};
+	
+	public Treinador(String nome){
+		this.nome = nome;
+	}
+	
+	public void euEscolhoVoce(){
+		pokemonAtual = pokemons[nxt++];
+		System.out.println(nome+" escolhe pokemon "+pokemonAtual.nome);
+	}
+	
+	public void defineAtaque(Pokemon adversario){
+		int n;
+		int danoParcial;
+		dano = -1;
+		for(int i=0;i<4;i++){
+			n = pokemonAtual.ataques[i].tipo;
+			danoParcial = (int) (efectividade[n][adversario.tipo])*(pokemonAtual.ataques[i].dano);
+			if(danoParcial > dano){
+				dano = danoParcial;
+				ataqueAtual = pokemonAtual.ataques[i].nome;
+			}
+		}
+	}
+	
+	public int oQueFarei(){
+		if(nxt == 6){
+			return 0;
+		}
+		
+		else if(pokemonAtual.atualHP == 0){
+			System.out.println("O pokemon "+pokemonAtual.nome+" foi  gravemente ferido!");
+			euEscolhoVoce();
+			return 1;
+		}
+		
+		else if(pokemonAtual.atualHP <= 10){
+			pokemonAtual.cura();
+			return 2;
+		}
+		
+		else{
+			return 3;
+		}
+		
+	}
+	
+}
+
+class Treinador1 extends Treinador{
+	
+	
+	public Treinador1(String nome) {	
+		super(nome);
+		pokemons[0] = new Bulbasaur("Bulba");
+		pokemons[1] = new Snorlax("Snor");
+		pokemons[2] = new Mewtwo("Mew");
+		pokemons[3] = new Pikachu("Pika");
+		pokemons[4] = new Butterfree("Butter");
+		pokemons[5] = new Pidgeot("Pidg");
+	}
+	
+}
+
+class Treinador2 extends Treinador{
+	
+	public Treinador2(String nome) {
+		super(nome);
+		pokemons[0] = new Hitmonchan("Hitmon");
+		pokemons[1] = new Charizard("Chari");
+		pokemons[2] = new Wartortle("Marto");
+		pokemons[3] = new Jigglypuff("Jiggly");
+		pokemons[4] = new Meowth("Meowth");
+		pokemons[5] = new Lapras("Lapras");
+	}
+	
+}
